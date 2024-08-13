a6162593917554569baf08f6f66a1dcb9b8f1dfb
DINHUYEN <uyenthitnuong@gmail.com>
2024-08-13 15:49:02 +0900

    Assignment-03

--- src/main/java/Group.java
+++ src/main/java/Group.java
@@ -7,4 +7,27 @@ public class Group {
     Account creator;
     LocalDate createdDate;
     Account[] accounts;
+
+    //Testing System4 = Question3
+    Group(){
+    }
+    Group(String name, Account creator, Account[] accounts, LocalDate createdDate){
+        this.name =name;
+        this.creator = creator;
+        this.accounts = accounts;
+        this.createdDate =createdDate;
+    }
+
+    Group(String name, Account creator, String[] usernames, LocalDate createdDate){
+        this.name = name;
+        this.creator = creator;
+        Account[] acc = new Account[usernames.length];
+        for (int i =0; i < usernames.length; i++){
+            Account a = new Account();
+            a.username = usernames[i];
+            acc[i] = a;
+        }
+        this.accounts = acc;
+        this.createdDate =createdDate;
+    }
 }
